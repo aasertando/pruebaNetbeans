dbbec8560dcf5c084d7efa450acd1e6e55feac05
aser <aser@Linux-Aser>
2026-09-24 21:31:18 -0500

    se crea una clase empleado y se colcoa un comentario de prueba

--- src/githubprueba/GithubPrueba.java
+++ src/githubprueba/GithubPrueba.java
@@ -15,6 +15,10 @@ public class GithubPrueba {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        
+        //ola siksjhsdjhfj
+        
     }
     
 }
